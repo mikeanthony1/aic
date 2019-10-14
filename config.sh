aceeae49966177f3e73e003130fb092578684f7e
Mike Anthony <themiketony@gmail.com>
2019-10-14 09:45:18 -0700

    Adding basic prereq checking

--- config.sh
+++ config.sh
@@ -1,43 +1,72 @@
 #!/bin/sh
 
 #Install dependencies
-apt install -y make gcc wget unzip
+apt install -y make gcc wget unzip curl
  
 #Download Source
-wget https://github.com/projectceladon/kernel-modules-cic/archive/master.zip
-unzip master.zip
+FILE=master.zip
+if [ ! -f "$FILE" ]; then
+	wget https://github.com/projectceladon/kernel-modules-cic/archive/master.zip
+	unzip $FILE
+fi
  
-#Make kernel modules
-cd kernel-modules-cic-master
-make -C ashmem -j `nproc`
-make -C binder -j `nproc`
+#TODO Ugly checking... improve
+if ! lsmod | grep ashmem; then
+	#Make kernel modules
+	cd kernel-modules-cic-master
+	make -C ashmem -j `nproc`
+	make -C binder -j `nproc`
+
  
-#Create library destination
-export DESTDIR=/lib/modules/`uname -r`/extra
-mkdir -p $DESTDIR
+	#Create library destination
+	export DESTDIR=/lib/modules/`uname -r`/extra
+	mkdir -p $DESTDIR
  
-#Install kernel modules
-make -C ashmem -j `nproc` install
-make -C binder -j `nproc` install
-depmod
-printf "\nashmem_module\nbinder_module\nbinderfs_module\n" >> /etc/modules
+	#Install kernel modules
+	make -C ashmem -j `nproc` install
+	make -C binder -j `nproc` install
+	depmod
+	printf "\nashmem_module\nbinder_module\nbinderfs_module\n" >> /etc/modules
+else
+	 echo "Skipping ashmem/binder installation"
+fi
  
 #Get official Docker GPG key
-apt install -y curl
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+if ! hash docker 2> /dev/null; then
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-#Add Docker repo to apt resources and update package database
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge"
-apt-get update
+	#Add Docker repo to apt resources and update package database
+	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge"
+	apt-get update
 
-#This step can be used to check all previous steps worked
-apt-cache policy docker-ce
+	#This step can be used to check all previous steps worked
+	apt-cache policy docker-ce
 
-#Install Docker
-apt-get install -y docker-ce
+	#Install Docker
+	apt-get install -y docker-ce
+else
+	echo "Skipping Docker Install"
+fi
 
-#Get Compose Binary
-curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+if ! hash docker-compose 2> /dev/null; then
+	#Get Compose Binary
+	curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-#Set run attribute
-chmod +x /usr/local/bin/docker-compose
+	#Set run attribute
+	chmod +x /usr/local/bin/docker-compose
+else
+	echo "Skipping docker-compose install"
+fi
+
+CPU_PATH="$(pwd)/workdir/cpu"
+if [ ! -d $CPU_PATH ]; then
+	echo "Making cpu path at: $CPU_PATH"
+	mkdir -p $CPU_PATH
+fi
+
+APP_DIR="$(pwd)/workdir/app/installed"
+if [ ! -d $APP_DIR ]; then
+	echo "Making cpu path at: $APP_DIR"
+	mkdir -p $APP_DIR
+fi
+
